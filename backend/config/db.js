9d278afb6de426a5ef8a35070b9ff454077e2b79
gabriell3103 <g5819853@gmail.com>
2025-05-27 15:02:24 -0300

    Correção da conexão do banco de dados

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const dbPassword = process.env.DB_PASS;
 const conn = async () => {
   try {
     const dbConn = await mongoose.connect(
-      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.ljrag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.pwk8b.mongodb.net/`
     );
     console.log("Conectou ao banco de dados!");
 
